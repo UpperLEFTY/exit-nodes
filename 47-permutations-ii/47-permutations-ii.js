1086d60dbced3cd103cd0291db1ff36c1ca497f0
Essence Alexander <upperlefty@zoho.eu>
2022-02-11 13:24:42 -0800

    Time: 118 ms (64.34%), Space: 46.6 MB (25.49%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    let res = [];
+    nums.sort((a, b) => (a - b));
+    dfs(res, [], nums);
+        return res;
+};
+
+const dfs = function (res, arr, nums) {
+  let len  = nums.length;
+  let tmp1 = null;
+  let tmp2 = null;
+    
+ if (!len) return res.push(arr);
+    
+    for (let i = 0; i < len; i++) {
+    if (nums[i] === nums[i - 1]) continue;
+
+    tmp1 = Array.from(arr);
+    tmp1.push(nums[i]);
+
+    tmp2 = Array.from(nums);
+    tmp2.splice(i, 1);
+
+    dfs(res, tmp1, tmp2);
+ }
+};
