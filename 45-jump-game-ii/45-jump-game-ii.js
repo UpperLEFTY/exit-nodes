9dd5ba7cbb91152202525089fb40adf84700aebd
Essence Alexander <upperlefty@zoho.eu>
2022-02-14 20:31:39 -0800

    Time: 63 ms (98.77%), Space: 44.3 MB (23.16%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    const len = nums.length;
+    let step = 0;
+    let now = 0;
+    let max = 0;
+    
+    for(let i = 0;i < len - 1; i++) {
+    max = Math.max(max, i + nums[i]);
+    if (i === now) {
+      step++;
+      now = max;
+    }
+  }
+
+  return step;
+}; 
+    
