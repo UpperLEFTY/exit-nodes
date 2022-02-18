9a740e92aa20efaca40b3ab064e23a28ac1eb253
Essence Alexander <upperlefty@zoho.eu>
2022-02-18 13:50:21 -0800

    Time: 65 ms (83.85%), Space: 41.6 MB (57.58%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(node) {
+  head = new ListNode(-1);
+  let Ln = head;
+
+  while (node !== null) {
+    if (node.next !== null) {
+      let one = node;
+      let two = node.next;
+      let three = node.next.next;
+      Ln.next = two;
+      two.next = one;
+      one.next = three;
+      Ln = Ln.next.next;
+      node = three;
+    } else {
+      Ln.next = node;
+      break;
+    }
+  }
+
+  return head.next;
+};
