5ce7fe68c3f5ec53aa3a6df09fb44d798faacc45
Essence Alexander <upperlefty@zoho.eu>
2022-02-18 13:55:31 -0800

    Time: 120 ms (43.75%), Space: 67.6 MB (12.50%) - LeetHub

---
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestRepeatingSubstring = function(s) {
+   const n = s.length;
+    const dp = Array.from({ length: n + 1 }, () => Array(n + 1).fill(0));
+    let res = 0;
+    for (let i = 1; i <= n; i++) {
+        for (let j = i + 1; j <= n; j++) {
+            if (s.charAt(i - 1) === s.charAt(j - 1)) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                res = Math.max(res, dp[i][j]);
+            }
+        }
+    }
+    return res;
+};
