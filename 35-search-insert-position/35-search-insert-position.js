aa375065c3f53b6be40eea6e876103932839703f
Essence Alexander <upperlefty@zoho.eu>
2022-02-22 16:48:28 -0800

    Time: 80 ms (62.12%), Space: 42.2 MB (25.18%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    const len = nums.length;
+    let left = 0;
+    let right = len - 1;
+    let mid = 0;
+    
+    if (!len) return 0;
+    
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+        if (nums[mid] > target) {
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return (nums[mid] > target) ? mid : (mid + 1);
+};
