fd55d9b917d27d88a4075b68efa7eeb78d87f413
Essence Alexander <upperlefty@zoho.eu>
2022-02-22 16:28:32 -0800

    Time: 60 ms (95.65%), Space: 41.9 MB (24.70%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+ const hash = {
+    0: 1,
+    1: 1
+  }
+  return valNumTrees(n, hash)
+}
+
+function valNumTrees(n, hash) {
+  if (hash[n]) return hash[n]
+  let sum = 0
+  for (let i = 1; i <= n; i++) {
+    const left = valNumTrees(i - 1, hash)
+    const right = valNumTrees(n - i, hash)
+    sum += left * right
+  }
+  hash[n] = sum
+  return sum
+};
