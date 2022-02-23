78e43fa9d13f3fbfcb8f41ec6f90227cfcce6b34
Essence Alexander <upperlefty@zoho.eu>
2022-02-22 16:50:40 -0800

    Time: 90 ms (84.48%), Space: 50 MB (49.73%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    const len = nums.length;
+    let max = Number.MIN_SAFE_INTEGER;
+    let before = 0;
+    let now = 0;
+    
+    if (!len) return 0;
+    
+    for (let i = 0; i < len; i++) {
+        now = Math.max(before + nums[i], nums[i]);
+        max = Math.max(now, max);
+        before = now;
+    }
+    return max;
+};
