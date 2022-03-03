88874396e423cb1df8f9954265df0cead1c90f81
Essence Alexander <upperlefty@zoho.eu>
2022-03-03 05:26:57 -0800

    Time: 80 ms (99.11%), Space: 46.5 MB (43.90%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  let len = nums.length;
+  let lastZeroIndex = -1;
+  for (let i = 0; i < len; i++) {
+    if (nums[i] === 0 && lastZeroIndex === -1) {
+      lastZeroIndex = i;
+    } else if (nums[i] !== 0 && lastZeroIndex !== -1) {
+      swap(nums, i, lastZeroIndex);
+      lastZeroIndex += 1;
+    }
+  }
+};
+
+let swap = function(arr, i, j) {
+  let tmp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = tmp;
+};
