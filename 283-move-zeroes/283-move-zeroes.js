7884a56b8f117c17464f3b19f84aff78e6156eac
Essence Alexander <upperlefty@zoho.eu>
2022-03-03 05:28:02 -0800

    Time: 130 ms (57.37%), Space: 46.7 MB (29.92%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -2,21 +2,34 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-  let len = nums.length;
-  let lastZeroIndex = -1;
-  for (let i = 0; i < len; i++) {
-    if (nums[i] === 0 && lastZeroIndex === -1) {
-      lastZeroIndex = i;
-    } else if (nums[i] !== 0 && lastZeroIndex !== -1) {
-      swap(nums, i, lastZeroIndex);
-      lastZeroIndex += 1;
+var moveZeroes = function(nums) { 
+  const len = nums.length;
+  let l = len;
+  for (let i = 0; i < l; ) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+      l -= 1;
+    } else {
+      i += 1;
     }
   }
 };
 
-let swap = function(arr, i, j) {
-  let tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
-};
+//   let len = nums.length;
+//   let lastZeroIndex = -1;
+//   for (let i = 0; i < len; i++) {
+//     if (nums[i] === 0 && lastZeroIndex === -1) {
+//       lastZeroIndex = i;
+//     } else if (nums[i] !== 0 && lastZeroIndex !== -1) {
+//       swap(nums, i, lastZeroIndex);
+//       lastZeroIndex += 1;
+//     }
+//   }
+// };
+
+// let swap = function(arr, i, j) {
+//   let tmp = arr[i];
+//   arr[i] = arr[j];
+//   arr[j] = tmp;
+// };
