2196c64cf2158e3255ee8ff2a7382a0f05782606
Essence Alexander <upperlefty@zoho.eu>
2022-03-06 21:19:46 -0800

    Time: 138 ms (70.68%), Space: 57.8 MB (27.78%) - LeetHub

---
+++ 652-find-duplicate-subtrees/652-find-duplicate-subtrees.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode[]}
+ */
+var findDuplicateSubtrees = function(root) {
+ const hash = {}, res = []
+  pre(root, hash, res)
+  return res
+};
+
+function pre(node, hash, res) {
+  if(node == null) return '#'
+  const str = `${node.val},${pre(node.left, hash, res)},${pre(node.right, hash, res)}`
+  if(hash[str] == null) hash[str] = 0
+  hash[str]++
+  if(hash[str] === 2) res.push(node)
+  return str
+}
