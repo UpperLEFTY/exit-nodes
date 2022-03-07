1726c64fe1d6b6c513a35245d954b0f289862879
Essence Alexander <upperlefty@zoho.eu>
2022-03-06 21:29:10 -0800

    Time: 104 ms (70.04%), Space: 45.9 MB (51.90%) - LeetHub

---
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var countSquares = function(matrix) {
+ const [m, n] = [matrix.length, matrix[0].length]
+  let res = 0
+  for(let i = 0;  i < m; i++) {
+    for(let j = 0; j < n; j++) {
+      if(matrix[i][j] && i > 0 && j > 0) {
+        matrix[i][j] = 1 + Math.min(
+          matrix[i - 1][j],
+          matrix[i][j - 1],
+          matrix[i - 1][j - 1],
+        )
+      }
+      res += matrix[i][j]
+    }
+  }
+  return res
+};
