87d1a7038029c7fa532735d4e35ec577fb7567e2
Essence Alexander <upperlefty@zoho.eu>
2022-03-11 10:27:07 -0800

    Time: 64 ms (97.51%), Space: 43.2 MB (32.42%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -3,29 +3,49 @@
  * @return {number}
  */
 var trap = function(height) {
-    let res = 0;
-    let left = 0;
-    let right = height.length - 1;
-    let leftMax = 0;
-    let rightMax = 0;
-    
-    while (left < right) {
-    if (height[left] < height[right]) {
-      if (height[left] >= leftMax) {
-        leftMax = height[left];
+      const n = height.length
+    if(n === 0) return 0
+    let res = 0
+    let l = 0, r = n - 1, leftMax = height[l], rightMax = height[r]
+    while(l < r) {
+      if(height[l] <= height[r]) {
+        l++
+        leftMax = Math.max(leftMax, height[l])
+        res += (leftMax - height[l]) 
       } else {
-        res += leftMax - height[left];
+        r--
+        rightMax = Math.max(rightMax, height[r])
+        res += rightMax - height[r]
       }
-      left++;
-    } else {
-      if (height[right] >= rightMax) {
-        rightMax = height[right];
-      } else {
-        res += rightMax - height[right];
-      }
-      right--;
     }
-  }
 
-  return res;
+    return res
 };
+
+    
+//     let res = 0;
+//     let left = 0;
+//     let right = height.length - 1;
+//     let leftMax = 0;
+//     let rightMax = 0;
+    
+//     while (left < right) {
+//     if (height[left] < height[right]) {
+//       if (height[left] >= leftMax) {
+//         leftMax = height[left];
+//       } else {
+//         res += leftMax - height[left];
+//       }
+//       left++;
+//     } else {
+//       if (height[right] >= rightMax) {
+//         rightMax = height[right];
+//       } else {
+//         res += rightMax - height[right];
+//       }
+//       right--;
+//     }
+//   }
+
+//   return res;
+// };
