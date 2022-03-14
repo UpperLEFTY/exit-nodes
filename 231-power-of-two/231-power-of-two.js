9c8fc55b9bcfbbcefcf895528c5a8b1f9b2e18d6
Essence Alexander <upperlefty@zoho.eu>
2022-03-13 17:38:34 -0700

    Time: 105 ms (48.88%), Space: 44.1 MB (13.81%) - LeetHub

--- 231-power-of-two/231-power-of-two.js
+++ 231-power-of-two/231-power-of-two.js
@@ -2,6 +2,8 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfTwo = function(n) {
-    return Math.log2(n)%1 === 0
-};
+// var isPowerOfTwo = function(n) {
+//     return Math.log2(n)%1 === 0
+// };
+
+const isPowerOfTwo = n => n < 1 ? false : Number.MAX_VALUE % n === 0
