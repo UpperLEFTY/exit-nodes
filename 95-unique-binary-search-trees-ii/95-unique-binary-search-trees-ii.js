9b025b800b6218d66ebbe6b57e952901bf17f454
Essence Alexander <upperlefty@zoho.eu>
2022-03-14 06:56:01 -0700

    Time: 116 ms (62.58%), Space: 48.8 MB (23.27%) - LeetHub

---
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+var generateTrees = function(n) {
+     if (n === 0) return []
+ return genTreeList(1, n)
+}
+function genTreeList(start, end) {
+ const list = []
+ if (start > end) list.push(null)
+ for (let idx = start; idx <= end; idx++) {
+   const leftList = genTreeList(start, idx - 1)
+   const rightList = genTreeList(idx + 1, end)
+   for (let left of leftList) {
+     for (let right of rightList) {
+       const root = new TreeNode(idx)
+       root.left = left
+       root.right = right
+       list.push(root)
+     }
+   }
+ }
+ return list
+}
