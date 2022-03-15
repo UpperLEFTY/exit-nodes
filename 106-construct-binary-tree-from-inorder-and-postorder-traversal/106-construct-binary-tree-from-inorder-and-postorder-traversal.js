9a1339028d35d2f5e7722a5ef1c85cfc041622cb
Essence Alexander <upperlefty@zoho.eu>
2022-03-15 07:53:33 -0700

    Time: 106 ms (71.70%), Space: 45.7 MB (45.28%) - LeetHub

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function(inorder, postorder) {
+const hashmap = {};
+const posts = postorder;
+for (let i = 0; i < inorder.length; i++) {
+  hashmap[inorder[i]] = i;
+}
+return helper(postorder.length - 1, 0, inorder.length - 1);
+function helper(postEnd, inStart, inEnd) {
+  if (postEnd < 0 || inEnd < inStart) return null;
+  const val = posts[postEnd];
+  const idx = hashmap["" + val];
+  const root = new TreeNode(val);
+  root.left = helper(postEnd - (inEnd - idx) - 1, inStart, idx - 1);
+  root.right = helper(postEnd - 1, idx + 1, inEnd);
+  return root;
+  }
+};
