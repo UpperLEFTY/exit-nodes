793bc1ca29548bfb143f524fc0cf8e672c7f23b7
Essence Alexander <upperlefty@zoho.eu>
2022-03-27 00:13:07 -0700

    Time: 387 ms (52.76%), Space: 0B (100.00%) - LeetHub

---
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT Name AS Employee FROM Employee AS E,
+(SELECT DISTINCT Id, Salary FROM Employee) AS M
+WHERE E.ManagerId = M.Id AND E.Salary > M.Salary
+
