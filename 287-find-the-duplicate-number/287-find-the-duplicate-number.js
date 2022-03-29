5abfb8b19aba63603b7b6e46562e8667d19caae8
Essence Alexander <upperlefty@zoho.eu>
2022-03-29 07:07:24 -0700

    Time: 138 ms (47.81%), Space: 49.6 MB (88.38%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -3,12 +3,30 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
- const hash = {};
- for (let i = 0; i < nums.length; i++) {
-   if (hash.hasOwnProperty(nums[i])) {
-     return nums[i];
-   } else {
-     hash[nums[i]] = 1;
-   }
- }
+//  const hash = {};
+//  for (let i = 0; i < nums.length; i++) {
+//    if (hash.hasOwnProperty(nums[i])) {
+//      return nums[i];
+//    } else {
+//      hash[nums[i]] = 1;
+//    }
+//  }
+// };
+    
+  const len = nums.length
+  if(len > 0) {
+    let slow = nums[0]
+    let fast = nums[nums[0]]
+    while(slow !== fast) {
+      slow = nums[slow]
+      fast = nums[nums[fast]]
+    }
+    slow = 0
+    while(slow !== fast) {
+      slow = nums[slow]
+      fast = nums[fast]
+    }
+    return slow
+  }
+  return -1;
 };
