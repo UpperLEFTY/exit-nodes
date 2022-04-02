9ee6493d4c6c8f78a18d2f0ad6d81a225a06ebcf
Essence Alexander <upperlefty@zoho.eu>
2022-04-02 15:14:31 -0700

    Time: 101 ms (68.75%), Space: 47.9 MB (68.08%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let start = 0;
+     let end = s.length - 1;
+    const isPalindrome = function(start, end, removed) {
+   while (start <= end) {
+     if (s[start] !== s[end]) {
+       if (removed) {
+         return false;
+       }
+       return (
+         isPalindrome(start + 1, end, true) ||
+         isPalindrome(start, end - 1, true)
+       );
+     }
+     start++;
+     end--;
+   }
+   return true;
+ };
+ return isPalindrome(start, end, false);
+};
