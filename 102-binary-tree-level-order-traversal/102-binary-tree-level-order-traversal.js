a833e64238c72103039986e408a7bc0cde0c5e15
Essence Alexander <upperlefty@zoho.eu>
2022-05-19 08:30:23 -0700

    Time: 101 ms (26.82%), Space: 44.1 MB (80.96%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+     const res = [];
+  if (root == null) return res;
+  let next = [root];
+  while (next.length > 0) {
+    next = tr(res, next);
+  }
+  return res;
+};
+
+function tr(res, nodeArr) {
+  const arr = [];
+  const nextLevelNodes = [];
+  for (let i = 0; i < nodeArr.length; i++) {
+    arr.push(nodeArr[i].val);
+    if (nodeArr[i].left) {
+      nextLevelNodes.push(nodeArr[i].left);
+    }
+    if (nodeArr[i].right) {
+      nextLevelNodes.push(nodeArr[i].right);
+    }
+  }
+  if (arr.length) res.push(arr);
+  return nextLevelNodes;
+};
