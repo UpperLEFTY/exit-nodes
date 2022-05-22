f1e04091d0c880981883b150d22aaafabc05b5e5
Essence Alexander <upperlefty@zoho.eu>
2022-05-22 15:54:04 -0700

    Time: 73 ms (87.94%), Space: 42.2 MB (96.37%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+  let count = 0;
+ if (s == null || s.length === 0) {
+   return 0;
+ }
+ for (let i = 0; i < s.length; i++) {
+   extendPalindrome(s, i, i);
+   extendPalindrome(s, i, i + 1);
+ }
+ function extendPalindrome(str, left, right) {
+   while (
+     left >= 0 &&
+     right < s.length &&
+     s.charAt(left) === s.charAt(right)
+   ) {
+     count++;
+     left--;
+     right++;
+   }
+ }
+ return count;
+};
