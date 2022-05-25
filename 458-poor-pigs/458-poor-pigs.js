3c972d7e9bb8a855b3bd0d80e85a92d97cc7421a
Essence Alexander <upperlefty@zoho.eu>
2022-05-25 09:55:29 -0700

    Time: 90 ms (16.67%), Space: 42 MB (8.33%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} buckets
+ * @param {number} minutesToDie
+ * @param {number} minutesToTest
+ * @return {number}
+ */
+var poorPigs = function(buckets, minutesToDie, minutesToTest) {
+  const index = Math.ceil(minutesToTest / minutesToDie) + 1
+return Math.ceil(Math.log(buckets) / Math.log(index)) 
+};
