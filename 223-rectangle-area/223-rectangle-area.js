4c59beb2691b7023b98b8b67ed4e790270bd3111
Essence Alexander <upperlefty@zoho.eu>
2022-05-26 06:46:23 -0700

    Time: 223 ms (13.42%), Space: 47.5 MB (29.27%) - LeetHub

---
+++ 223-rectangle-area/223-rectangle-area.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} A
+* @param {number} B
+* @param {number} C
+* @param {number} D
+* @param {number} E
+* @param {number} F
+* @param {number} G
+* @param {number} H
+* @return {number}
+ */
+const computeArea = function(A, B, C, D, E, F, G, H) {
+ const areaA = (C - A) * (D - B)
+const areaB = (G - E) * (H - F)
+const intersectionArea =
+  Math.max(0, Math.min(C, G) - Math.max(A, E)) *
+  Math.max(0, Math.min(D, H) - Math.max(B, F))
+return areaA + areaB - intersectionArea  
+};
