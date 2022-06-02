939256f9bddcdfb7543b374e4d50bd0d9441e446
Essence Alexander <upperlefty@zoho.eu>
2022-06-02 10:53:07 -0700

    Time: 126 ms (5.55%), Space: 45 MB (60.20%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,5 +4,14 @@
  * @return {number}
  */
 var search = function(nums, target) {
-  return nums.indexOf(target)
+  let start = 0;
+let end = nums.length - 1;
+    
+while(start <= end){
+    let mid = parseInt((start + end) / 2);
+    if(nums[mid] === target) return mid;
+    if(nums[mid] > target) end = mid -1;
+    if(nums[mid] < target) start = mid + 1;
+}
+return -1;  
 };
