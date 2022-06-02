18cb75598923a8d73c5a47ab1871d43a16b646cb
Essence Alexander <upperlefty@zoho.eu>
2022-06-02 10:21:32 -0700

    Time: 85 ms (66.25%), Space: 44.5 MB (73.44%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -3,7 +3,9 @@
  * @return {number[][]}
  */
 var transpose = function(matrix) {
-  return matrix.reduce((prev, next) => next.map((item, i) =>
-    (prev[i] || []).concat(next[i])
-  ), []);
-};
+//   return matrix.reduce((prev, next) => next.map((item, i) =>
+//     (prev[i] || []).concat(next[i])
+//   ), []);
+// };
+return matrix[0].map((col, i) => matrix.map(row => row[i]));
+}
