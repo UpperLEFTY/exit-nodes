3fd02234dafc1e92ef80ded25872f69e41a1f9b4
Essence Alexander <upperlefty@zoho.eu>
2022-06-03 16:11:01 -0700

    Time: 368 ms (5.88%), Space: 49.1 MB (18.58%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -3,44 +3,18 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    nums = nums.map(num => num * num);
-    quickSort(nums, 0, nums.length - 1);
-    return nums;
-};
-
-function quickSort(nums, left, right) {
-    let index;
-    if (nums.length > 1) {
-        // index returned from partition
-        index = partition(nums, left, right);
-        // more elements on the left side of the pivot
-        if (left < index - 1)
-            quickSort(nums, left, index - 1);
-        // more elements on the right side of the pivot
-        if (index < right)
-            quickSort(nums, index, right);
+   const result = [];
+  let i = nums.length - 1;
+  let left = 0;
+  let right = nums.length -1;
+  while (left <= right) {
+    if (Math.abs(nums[left]) < nums[right]) {
+      result.unshift(nums[right] * nums[right])
+      right--;
+    } else {
+      result.unshift(nums[left] * nums[left])
+      left++
     }
-    // you can return or not return, 
-    // since we change nums, no need to return
-    // return nums;
-}
-
-function partition(nums, start, end) {
-    let l = start, r = end;
-    let mid = Math.floor((start + end) / 2);
-    let pivot = nums[mid];
-    // Important: l <= r not l < r
-    while (l <= r) {
-        while(nums[l] < pivot)
-            l++;
-        while(nums[r] > pivot)
-            r--;
-        if (l <= r) {
-            // swap
-            [nums[l], nums[r]] = [nums[r], nums[l]];
-            l++;
-            r--;
-        }
-    }
-    return l;
+  }
+  return result;
 };
