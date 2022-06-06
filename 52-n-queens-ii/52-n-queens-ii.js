a2cb285a2dbeabca9081934c5b1b2e47186d380f
Essence Alexander <upperlefty@zoho.eu>
2022-06-06 11:34:05 -0700

    Time: 81 ms (67.34%), Space: 42.5 MB (77.17%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.js
+++ 52-n-queens-ii/52-n-queens-ii.js
@@ -3,24 +3,27 @@
  * @return {number}
  */
 var totalNQueens = function(n) {
-     let count = 0;
-   const done = Math.pow(2, n) - 1;
-const innerRecurse = function(ld, col, rd) {
-    //if columns are occupied,
-    //so the solution must be complete
-    if (col === done) {
-      count++;
-      return;
-    }
-let poss = ~(ld | rd | col);
-    while (poss & done) {
-      let bit = poss & -poss;
-      poss -= bit;
-      innerRecurse((ld | bit) >> 1, col | bit, (rd | bit) << 1);
-    }
-  };
-
-  innerRecurse(0, 0, 0);
-
-  return count;
+   let count = 0;
+   const calc = Math.pow(2, n) - 1;
+    
+   const innerRecurse = function(ld, col, rd) {
+  //All columns are occupied,
+  //so the solution must be complete
+  if (col === calc) {
+    count++;
+    return;
+  }
+  //Gets a bit sequence with "1"s
+  //whereever there is an open "slot"
+  let poss = ~(ld | rd | col);
+  //Loops as long as there is a valid
+  //place to put another queen.
+  while (poss & calc) {
+    let bit = poss & -poss;
+    poss -= bit;
+    innerRecurse((ld | bit) >> 1, col | bit, (rd | bit) << 1);
+  }
 };
+innerRecurse(0, 0, 0);
+return count;
+}
