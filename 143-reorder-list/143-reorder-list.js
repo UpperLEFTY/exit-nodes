3bac235a7964fb1d6ff3c132dfca2dc3070a227e
Essence Alexander <upperlefty@zoho.eu>
2022-06-14 07:06:03 -0700

    Time: 153 ms (26.60%), Space: 49.9 MB (74.80%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+   if(head == null) return head
+ let slow = head, fast = head
+ while(fast && fast.next) {
+   slow = slow.next
+   fast = fast.next.next
+ }
+ let head2 = reverse(slow.next)
+ slow.next = null
+ 
+ while(head && head2) {
+   const next = head.next, next2 = head2.next
+   head2.next = head.next
+   head.next = head2
+   head = next
+   head2 = next2
+ }
+ 
+ function reverse(node) {
+   let pre = null, cur = node
+   while(cur) {
+     const tmp = cur.next
+     cur.next = pre
+     pre = cur
+     cur = tmp
+   }
+   return pre
+ } 
+};
