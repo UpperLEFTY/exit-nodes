9da581ad50ae24a703ff8dfadeadad88dc5e109a
Essence Alexander <upperlefty@zoho.eu>
2022-06-15 08:54:37 -0700

    Time: 157 ms (5.13%), Space: 44.2 MB (65.63%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -2,27 +2,24 @@
  * @param {number[][]} image
  * @param {number} sr
  * @param {number} sc
- * @param {number} newColor
+ * @param {number} color
  * @return {number[][]}
  */
-var floodFill = function(image, sr, sc, newColor, firstColor = image[sr][sc]) {
- if (
-    sr < 0 ||
-    sc < 0 ||
-    sr >= image.length ||
-    sc >= image[sr].length ||
-    image[sr][sc] !== firstColor ||
-    image[sr][sc] === newColor
-  ) {
-    return image
-  }
-
-  image[sr][sc] = newColor
-
-  floodFill(image, sr + 1, sc, newColor, firstColor)
-  floodFill(image, sr - 1, sc, newColor, firstColor)
-  floodFill(image, sr, sc + 1, newColor, firstColor)
-  floodFill(image, sr, sc - 1, newColor, firstColor)
-
+const floodFill = function(image, sr, sc, newColor, firstColor = image[sr][sc]) {
+  if (
+  sr < 0 ||
+  sc < 0 ||
+  sr >= image.length ||
+  sc >= image[sr].length ||
+  image[sr][sc] !== firstColor ||
+  image[sr][sc] === newColor
+) {
   return image
 }
+image[sr][sc] = newColor
+floodFill(image, sr + 1, sc, newColor, firstColor)
+floodFill(image, sr - 1, sc, newColor, firstColor)
+floodFill(image, sr, sc + 1, newColor, firstColor)
+floodFill(image, sr, sc - 1, newColor, firstColor)
+return image 
+};
