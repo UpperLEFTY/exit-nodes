717cc272ae44a864e38ef863f8f4c3b99614084a
Essence Alexander <upperlefty@zoho.eu>
2022-06-15 07:51:20 -0700

    Time: 110 ms (25.06%), Space: 43.9 MB (68.80%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.js
+++ 190-reverse-bits/190-reverse-bits.js
@@ -3,9 +3,22 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
- const b = n.toString(2)
- const leadingZeroes = b.padStart(32,'0')
- const rev = leadingZeroes.split('')
- rev.reverse()
- return parseInt(rev.join(''), 2)
-};
+//  const b = n.toString(2)
+//  const leadingZeroes = b.padStart(32,'0')
+//  const rev = leadingZeroes.split('')
+//  rev.reverse()
+//  return parseInt(rev.join(''), 2)
+// };
+    
+    // Another Solution JS
+    
+let s = '';
+let count = 0;
+let index = 31;
+while (n > 0) {
+  if (n % 2 !== 0) count += Math.pow(2, index);
+  index--;
+  n = Math.floor(n / 2);
+}
+return count;
+}
