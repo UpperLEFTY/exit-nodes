f67b639d6ea2ad7ad187ddba5df34a93d9bc7213
Essence Alexander <upperlefty@zoho.eu>
2022-06-15 07:53:08 -0700

    Time: 79 ms (82.55%), Space: 43.4 MB (77.25%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    return nums.reduce((ac, e) => ac ^ e, 0)
+//     return nums.reduce((ac, e) => ac ^ e, 0)
+// };
+    
+    // Another Solution JS
+    
+  let xor = nums[0]
+  for(let i = 1; i< nums.length; i++) xor ^= nums[i]
+  return xor
 };
