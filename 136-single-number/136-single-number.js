3cce4935c37bd5eeed82dbce2ffd220d984ea555
Essence Alexander <upperlefty@zoho.eu>
2022-06-15 07:52:19 -0700

    Time: 107 ms (47.19%), Space: 43.5 MB (71.76%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -3,10 +3,5 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let res = 0;
-    let len = nums.length;
-    for (let i = 0; i < len; i++) {
-        res ^= nums[i];
-    }
-    return res;
+    return nums.reduce((ac, e) => ac ^ e, 0)
 };
