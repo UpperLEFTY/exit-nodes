71ed0acf3396b4cfb6a6cd26e87a3d18f47cc727
Essence Alexander <upperlefty@zoho.eu>
2022-06-15 08:19:13 -0700

    Time: 130 ms (10.70%), Space: 44.1 MB (71.45%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -6,24 +6,26 @@
  * }
  */
 /**
- * @param {ListNode} list1
- * @param {ListNode} list2
+ * @param {ListNode} l1
+ * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-   const head = new ListNode(0);
-    let now = head;
-    let p1 = list1;
-    let p2 = list2;
-    while (p1 || p2) {
-        if (p1 === null || (p2 !== null && p2.val < p1.val)) {
-            now.next = p2;
-            p2 = p2.next;
-        } else {
-            now.next = p1;
-            p1 = p1.next;
-        }
-        now = now.next;
+const mergeTwoLists = function(l1, l2) {
+  const dummy = new ListNode()
+  let cur = dummy
+  while(l1 && l2) {
+    if(l1.val < l2.val) {
+      cur.next = new ListNode(l1.val)
+      l1 = l1.next
+    } else {
+      cur.next = new ListNode(l2.val)
+      l2 = l2.next
     }
-    return head.next;
+    
+    cur = cur.next
+  }
+  if(l1) cur.next = l1
+  if(l2) cur.next = l2
+  
+  return dummy.next
 };
