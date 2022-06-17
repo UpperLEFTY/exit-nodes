dafda6211d110d50455917a5f8720cde4dda9757
Essence Alexander <upperlefty@zoho.eu>
2022-06-17 09:07:52 -0700

    Time: 55 ms (98.56%), Space: 42.7 MB (50.70%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -3,27 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
-var searchRange = function(nums, target) {
-  let res = [-1, -1];
-  let left = find(nums, target, true);
-  let right = find(nums, target, false);
-  if (!nums.length) return res;
-  if (left > right) return res;
-  return [left, right];
-};
-
-  const find = function (nums, target, findLeft) {
-  let left = 0;
-  let right = nums.length - 1;
-  let mid = 0;
-      
-    while (left <= right) {
-    mid = Math.floor((left + right) / 2);
-    if (nums[mid] > target || (findLeft && nums[mid] === target)) {
-      right = mid - 1;
-    } else {
-      left = mid + 1;
-    }
+const searchRange = function(nums, target) {
+  let len = nums.length;
+  let start = 0;
+  let end = len - 1;
+  let res = [];
+  let idx;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (target === nums[mid]) {
+      idx = mid;
+      break;
+    } else if (target < nums[mid]) end = mid - 1;
+    else start = mid + 1;
   }
-  return findLeft ? left : right;
+  if (idx == null) return [-1, -1];
+  let li = idx;
+  let hi = idx;
+  while (nums[li - 1] === target) {
+    li--;
+  }
+  while (nums[hi + 1] === target) {
+    hi++;
+  }
+  res = [li, hi];
+  return res;
 };
