45ade19b49e895fc343b7fa3b186e9f7bdd3899a
Essence Alexander <upperlefty@zoho.eu>
2022-06-19 14:55:44 -0700

    Time: 62 ms (91.43%), Space: 42 MB (69.05%) - LeetHub

---
+++ 274-h-index/274-h-index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+const hIndex = function(citations) {
+const n = citations.length
+const arr = Array(n + 1).fill(0)
+for(let e of citations) {
+  if(e >= n) arr[n]++
+  else arr[e]++
+}
+for(let i = n, sum = 0; i >= 0; i--) {
+  sum += arr[i]
+  if(sum >= i) return i
+}
+return 0
+};
