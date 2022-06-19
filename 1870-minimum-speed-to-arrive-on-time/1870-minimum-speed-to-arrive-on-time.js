a6308b1759f828a67f849fe3702f9c5b01b0d4a1
Essence Alexander <upperlefty@zoho.eu>
2022-06-19 13:48:15 -0700

    Time: 172 ms (85.71%), Space: 53.5 MB (85.71%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} dist
+ * @param {number} hour
+ * @return {number}
+ */
+const minSpeedOnTime = function(dist, hour) {
+  let n = dist.length, l = 1, r = 1e7 + 1
+  while(l < r) {
+    const mid = l + ((r - l) >> 1)
+    let time = 0
+    for(let i = 0; i < n - 1; i++) time += Math.ceil(dist[i] / mid)
+    time += dist[dist.length - 1] / mid
+    if(time > hour) l = mid + 1
+    else r = mid
+  }
+  return l > 1e7 ? -1 : l
+};
