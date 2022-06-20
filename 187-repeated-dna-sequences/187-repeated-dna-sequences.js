e76d6e0cf2717a05b71d6e2e7104eb2dc7f438b7
Essence Alexander <upperlefty@zoho.eu>
2022-06-20 09:40:42 -0700

    Time: 108 ms (81.27%), Space: 52.1 MB (94.18%) - LeetHub

---
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+const findRepeatedDnaSequences = function(s) {
+    let store = new Set(), result = new Set()
+    for(let i = 0; i < s.length - 9; i++) {
+        const str = s.substring(i, i + 10)
+        if(store.has(str)) {
+            result.add(str)
+        } else {
+            store.add(str)
+        }
+    }
+    return Array.from(result)
+};
