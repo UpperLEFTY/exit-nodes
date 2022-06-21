53793f45e1e75de3860c37550a178a7c2235c6f8
Essence Alexander <upperlefty@zoho.eu>
2022-06-21 11:15:32 -0700

    Time: 125 ms (5.05%), Space: 41.9 MB (78.79%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findPeakElement = function(nums) {
+    let low = 0; 
+    let high = nums.length-1;
+  while(low < high) {
+   let mid1 = low + ((high - low) >> 1);
+   let mid2 = mid1 + 1;
+   if(nums[mid1] < nums[mid2]) low = mid2;
+   else high = mid1;
+ }
+ return low;
+};
