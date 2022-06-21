1c0e5a4f6e68a36f7c3863ca38afc0266990383a
Essence Alexander <upperlefty@zoho.eu>
2022-06-21 11:17:52 -0700

    Time: 121 ms (17.48%), Space: 44.3 MB (68.33%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
@@ -9,24 +9,24 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
-  let dummy = new ListNode(undefined);
+const deleteDuplicates = function(head) {
+    let dummy = new ListNode(undefined);
     dummy.next = head;
     let prev = dummy;
-    let curr = head;
+    let curr = head; 
     
-    while(curr) {
-      while(curr.next && curr.next.val === curr.val) {
-        curr = curr.next;
-      }
-      if(prev.next === curr) { // detect if it has deleted some elements
-        prev = prev.next;
-        curr = curr.next;
-      } else {
-        prev.next = curr.next;
-        curr = curr.next;
-      }
-    }
-    
-    return dummy.next;
-  };
+     while(curr) {
+   while(curr.next && curr.next.val === curr.val) {
+     curr = curr.next;
+   }
+   if(prev.next === curr) { // if it has deleted some elements
+     prev = prev.next;
+     curr = curr.next;
+   } else {
+     prev.next = curr.next;
+     curr = curr.next;
+   }
+ }
+ 
+ return dummy.next;
+};
