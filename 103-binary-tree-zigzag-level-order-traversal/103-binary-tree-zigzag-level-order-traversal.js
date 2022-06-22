28a3f2521bc3f467dc773994712916f336384a05
Essence Alexander <upperlefty@zoho.eu>
2022-06-22 08:32:32 -0700

    Time: 82 ms (61.38%), Space: 43.6 MB (90.74%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
@@ -10,32 +10,51 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-const zigzagLevelOrder = function(root) {
-  if(root == null) return []
-  const row = [root]
+// const zigzagLevelOrder = function(root) {
+//   if(root == null) return []
+//   const row = [root]
+//   const res = []
+//   bfs(row, res)
+//   for(let i = 0; i < res.length; i++) {
+//     res[i] = i % 2 === 0 ? res[i] : res[i].reverse()
+//   }
+//   return res
+// };
+
+// function bfs(row, res) {
+//   if(row.length === 0) return
+//   let tmp = []
+//   let next = []
+//   for(let i = 0; i< row.length; i++) {
+//     tmp.push(row[i].val)
+//     if(row[i].left) {
+//        next.push(row[i].left)
+//     }
+//     if(row[i].right) {
+//        next.push(row[i].right)
+//     }
+//   }
+//   if(tmp.length) {
+//     res.push(tmp)
+//   }
+//   bfs(next, res)
+// }
+    // Another soultion JS
+
+const zigzagLevelOrder = function (root) {
   const res = []
-  bfs(row, res)
-  for(let i = 0; i < res.length; i++) {
-    res[i] = i % 2 === 0 ? res[i] : res[i].reverse()
-  }
+  dfs(root, res, 0)
   return res
+  
+  function dfs(node, res, level) {
+    if(node == null) return
+    if(res.length <= level) res.push([])
+    const tmp = res[level]
+    if(level % 2 === 0) tmp.push(node.val)
+    else tmp.unshift(node.val)
+    
+    dfs(node.left, res, level + 1)
+    dfs(node.right, res, level + 1)
+  }
 };
 
-function bfs(row, res) {
-  if(row.length === 0) return
-  let tmp = []
-  let next = []
-  for(let i = 0; i< row.length; i++) {
-    tmp.push(row[i].val)
-    if(row[i].left) {
-       next.push(row[i].left)
-    }
-    if(row[i].right) {
-       next.push(row[i].right)
-    }
-  }
-  if(tmp.length) {
-    res.push(tmp)
-  }
-  bfs(next, res)
-}
