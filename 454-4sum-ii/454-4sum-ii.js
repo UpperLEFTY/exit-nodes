1f8180a89e7302d4e7c8505bffa12c22cd638b87
Essence Alexander <upperlefty@zoho.eu>
2022-06-22 09:54:59 -0700

    Time: 310 ms (59.57%), Space: 46.2 MB (76.90%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+const fourSumCount = function(A, B, C, D) {
+    const map = new Map()
+    let result = 0
+   for (let i = 0, clen = C.length; i < clen; i++) {
+  for (let j = 0, dlen = D.length; j < dlen; j++) {
+    map.set(
+      C[i] + D[j],
+      typeof map.get(C[i] + D[j]) == 'undefined'
+        ? 1
+        : map.get(C[i] + D[j]) + 1
+    )
+  }
+}
+for (let i = 0, alen = A.length; i < alen; i++) {
+  for (let j = 0, blen = B.length; j < blen; j++) {
+    result +=
+      typeof map.get((A[i] + B[j]) * -1) == 'undefined'
+        ? 0
+        : map.get((A[i] + B[j]) * -1)
+  }
+}
+return result
+};
