ea75a70003517f37b79fc04f74f27b130b4475fd
Essence Alexander <upperlefty@zoho.eu>
2022-07-01 07:55:50 -0700

    Time: 72 ms (81.82%), Space: 42.2 MB (84.92%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const backspaceCompare = function(s, t) {
+    return chk(s) === chk(t)
+};
+
+function chk(str) {
+    const s = []
+    for(let i = 0, len = str.length; i < len; i++) {
+        if(str[i] === '#') s.pop()
+        else s.push(str[i])
+    }
+     return s.join('')
+}
