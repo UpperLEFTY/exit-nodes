2c7e9353c98dc9eac3a2aec13f7555ef5803b29a
Essence Alexander <upperlefty@zoho.eu>
2022-08-26 08:58:08 -0700

    Time: 70 ms (87.27%), Space: 41.8 MB (58.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -4,10 +4,10 @@
  */
 const cache = {}
 const fib = function(n) {
-  if(cache[n]) return cache[n]
-  if(n === 0) return 0
-  if(n === 1) return 1
-  let res = fib(n - 1) + fib(n - 2)
-  cache[n] = res
-  return res
+   if(cache[n]) return cache[n]
+   if(n === 0) return 0
+   if(n === 1) return 1
+    let res = fib(n - 1) + fib(n - 2)
+    cache[n] = res
+    return res
 };
