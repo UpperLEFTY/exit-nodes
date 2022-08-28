a77ca145bd1f4d0e98bf7c2355bb7e623bd2d596
Essence Alexander <upperlefty@zoho.eu>
2022-08-27 21:15:56 -0700

    Time: 71 ms (76.82%), Space: 42.1 MB (28.76%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -2,11 +2,25 @@
  * @param {number} n
  * @return {number}
  */
-const hash = {}
+ //const hash = {}
 const tribonacci = function(n) {
-  if(n === 0) return 0
-  if(n === 2 || n === 1) return 1
-  if(hash[n] != null) return hash[n]
-  let tmp = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1)
-  return hash[n] = tmp
-};
+//   if(n === 0) return 0
+//   if(n === 2 || n === 1) return 1
+//   if(hash[n] != null) return hash[n]
+//   let tmp = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1)
+//   return hash[n] = tmp
+// };
+
+if (n < 2) return n
+    let o = 0
+    let p = 1
+    let q = 1
+   for (let count = 3; count <= n; count++) {
+       let next = q + p + o
+       o = p
+       p = q
+       q = next
+   }
+    return q
+}
+   
