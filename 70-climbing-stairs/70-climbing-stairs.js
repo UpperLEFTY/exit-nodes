003f9f76da23d63376df4bd451aff981d353ca3a
Essence Alexander <upperlefty@zoho.eu>
2022-08-27 21:17:26 -0700

    Time: 62 ms (92.61%), Space: 41.4 MB (93.87%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
+const climbStairs = function(n) {
     let climb = [0, 1];
     for (let i = 0; i < n; i++) {
         climb = [climb[1], climb[0] + climb[1]];
