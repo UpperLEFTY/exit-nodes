429415e0877a7458b843317f206db0fe515c5c82
Essence Alexander <upperlefty@zoho.eu>
2022-08-29 08:24:03 -0700

    Time: 411 ms (18.82%), Space: 51.4 MB (41.19%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfThree = function(n) {
+    const maxInt = Math.pow(3,30)
+    if(n < 0) {
+        return false
+    }
+    return maxInt % n === 0
+};
