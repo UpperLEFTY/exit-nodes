8317431b42d108151e5724ad372a4b4cae4c011f
Essence Alexander <upperlefty@zoho.eu>
2022-09-16 06:53:39 -0700

    Time: 96 ms (48.41%), Space: 41.9 MB (90.49%) - LeetHub

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMin = function(nums) {
+     for(let i = 1, len = nums.length; i < len; i++) {
+   if(nums[i] < nums[i - 1]) {
+     return nums[i]
+   }
+ }
+ return nums[0]
+};
