02591a6a46f24c67c23e8254847f2df32aa7ff1b
Essence Alexander <upperlefty@zoho.eu>
2022-09-17 14:30:17 -0700

    Time: 62 ms (99.16%), Space: 44.7 MB (21.70%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.js
@@ -9,24 +9,50 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-const deleteDuplicates = function(head) {
-    let dummy = new ListNode(undefined);
-    dummy.next = head;
-    let prev = dummy;
-    let curr = head; 
+// const deleteDuplicates = function(head) {
+//     let dummy = new ListNode(undefined);
+//     dummy.next = head;
+//     let prev = dummy;
+//     let curr = head; 
     
-     while(curr) {
-   while(curr.next && curr.next.val === curr.val) {
-     curr = curr.next;
-   }
-   if(prev.next === curr) { // if it has deleted some elements
-     prev = prev.next;
-     curr = curr.next;
-   } else {
-     prev.next = curr.next;
-     curr = curr.next;
-   }
- }
+//      while(curr) {
+//    while(curr.next && curr.next.val === curr.val) {
+//      curr = curr.next;
+//    }
+//    if(prev.next === curr) { // if it has deleted some elements
+//      prev = prev.next;
+//      curr = curr.next;
+//    } else {
+//      prev.next = curr.next;
+//      curr = curr.next;
+//    }
+//  }
  
- return dummy.next;
-};
+//  return dummy.next;
+// };
+
+// another 
+
+
+const deleteDuplicates = function (head) {
+  const temp = {
+    next: head,
+  }
+  let prev = temp
+  let current = head
+  while (current) {
+    let isDuplicate = false
+    while (current.next && current.val === current.next.val) {
+      isDuplicate = true
+      current.next = current.next.next
+    }
+    current = current.next
+    if (isDuplicate) {
+      prev.next = current
+      isDuplicate = false
+    } else {
+      prev = prev.next
+    }
+  }
+  return temp.next
+}
