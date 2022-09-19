d857738bfaede9a1806ab29c6cbc769a242e715c
Essence Alexander <upperlefty@zoho.eu>
2022-09-19 10:50:34 -0700

    Time: 194 ms (5.44%), Space: 46.7 MB (12.55%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -2,50 +2,19 @@
  * @param {number[]} height
  * @return {number}
  */
-var trap = function(height) {
-      const n = height.length
-    if(n === 0) return 0
-    let res = 0
-    let l = 0, r = n - 1, leftMax = height[l], rightMax = height[r]
-    while(l < r) {
-      if(height[l] <= height[r]) {
-        l++
-        leftMax = Math.max(leftMax, height[l])
-        res += (leftMax - height[l]) 
-      } else {
-        r--
-        rightMax = Math.max(rightMax, height[r])
-        res += rightMax - height[r]
-      }
-    }
-
-    return res
-};
-
-    
-//     let res = 0;
-//     let left = 0;
-//     let right = height.length - 1;
-//     let leftMax = 0;
-//     let rightMax = 0;
-    
-//     while (left < right) {
-//     if (height[left] < height[right]) {
-//       if (height[left] >= leftMax) {
-//         leftMax = height[left];
-//       } else {
-//         res += leftMax - height[left];
-//       }
-//       left++;
-//     } else {
-//       if (height[right] >= rightMax) {
-//         rightMax = height[right];
-//       } else {
-//         res += rightMax - height[right];
-//       }
-//       right--;
-//     }
-//   }
-
-//   return res;
-// };
+const trap = function(height) {
+  const len = height.length
+  if (len === 0) return 0
+  const leftMax = [height[0]]
+  const rightMax = []
+  rightMax[len - 1] = height[len - 1]
+  for (let i = len - 2; i >= 0; i--) {
+    rightMax[i] = Math.max(height[i], rightMax[i + 1])
+  }
+  let res = 0
+  for (let i = 1; i < len; i++) {
+    leftMax[i] = Math.max(height[i], leftMax[i - 1])
+    res += Math.min(leftMax[i], rightMax[i]) - height[i]
+  }
+  return res
+}
