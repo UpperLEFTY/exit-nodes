8ba2f99658460359a5add2d5b2c598b9c1349a8d
Essence Alexander <upperlefty@zoho.eu>
2022-09-23 10:27:48 -0700

    Time: 112 ms (26.50%), Space: 44.5 MB (17.27%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsets = function(nums) {
-    const subs = [[]]
-    for (let num of nums) {
-        const n = subs.length
+ function subsets(nums) {
+  const subs = [[]]
+  for (let num of nums) {
+      const n = subs.length
       for (let i = 0; i < n; i++) {
           subs.push(subs[i].slice(0))
-          subs[subs.length -1].push(num)
+          subs[subs.length - 1].push(num)
       }
     }
     return subs
-}; 
+};
