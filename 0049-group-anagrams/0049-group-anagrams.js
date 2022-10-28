5b16a67fb79d62bb5432c9cbf74ac0d0644df986
Essence Alexander <upperlefty@zoho.eu>
2022-10-28 15:21:20 -0700

    Time: 193 ms (75.26%), Space: 53.9 MB (43.58%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+const groupAnagrams = function(strs) {
+   if (strs.length === 0)  {
+       return [];
+   }
+    const ans = [];
+    const hash = {};
+    for (let el of strs) {
+        let sel = el
+         .split("")
+         .sort()
+          .join("");
+        if (hash.hasOwnProperty(sel)) {
+            hash[sel].push(el);
+        } else {
+            hash[sel] = [el];
+        }
+    }
+    return Object.values(hash)
+};
