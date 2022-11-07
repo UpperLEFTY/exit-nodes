44e325e6507892edbaaec61d13926e04601f3151
Essence Alexander <upperlefty@zoho.eu>
2022-11-06 18:19:14 -0800

    Time: 87 ms (70.88%), Space: 41.5 MB (94.58%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const maximum69Number  = function(num) {
+ const arr = (num+'').split('')
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === '6') {
+      arr[i] = '9'
+      break
+    }
+  }
+  return arr.join('')
+};
+  
