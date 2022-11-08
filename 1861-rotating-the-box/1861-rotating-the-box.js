67cce26bff10b0026c8ec42097cdd2775baef2b3
Essence Alexander <upperlefty@zoho.eu>
2022-11-07 18:15:47 -0800

    Time: 432 ms (92.56%), Space: 85.2 MB (72.73%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.js
@@ -0,0 +1,21 @@
+/**
+ * @param {character[][]} box
+ * @return {character[][]}
+ */
+const rotateTheBox = function(box) {
+ const m = box.length, n = box[0].length
+  const res = Array.from({ length: n }, () => Array(m).fill('.'))
+  for(let i = 0; i < m; i++) {
+    for(let j = n - 1, pos = n - 1; j >= 0; j--) {
+      if(box[i][j] === '.') continue
+      if(box[i][j] === '#') {
+        res[pos][m - 1 - i] = '#'
+        pos--
+      } else {
+        res[j][m - 1 - i] = '*'
+        pos = j - 1
+      }
+    }
+  }
+  return res
+};
