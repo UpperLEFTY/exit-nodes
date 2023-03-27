a633c22d2827ac4b99b1151e61a8bee217ff42ea
Essence Alexander <upperlefty@zoho.eu>
2023-03-27 16:33:01 -0700

    Time: 174 ms (20.00%), Space: 48.9 MB (53.68%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.js
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.js
@@ -4,6 +4,18 @@
  * @return {number}
  */
 const rangeBitwiseAnd = function(left, right) {
-     while(left < right) right = right & (right - 1);
+ /**   while(left < right) right = right & (right - 1);
  return right;
+};
+*/
+    
+   /** another solution */
+    
+  let s = 0
+  while(left !== right) {
+    left >>= 1
+    right >>= 1
+    s++
+  }
+  return left << s
 };
