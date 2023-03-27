4208a9cd8ced557cdf43c20b10c9c5c8aa36b89e
Essence Alexander <upperlefty@zoho.eu>
2023-03-27 16:58:46 -0700

    Time: 85 ms (93.41%), Space: 49.7 MB (62.27%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const calculate = function(s) {
+const stack = [], n = s.length
+let op = '+', num = 0
+for(let i = 0; i < n; i++) {
+  const isNumber = s[i] >= '0' && s[i] <= '9'
+  if(isNumber) num = num * 10 + (+s[i])
+  if((!isNumber && s[i] !== ' ') || i === n - 1) {
+    if(op === '+') stack.push(num)
+    else if(op === '-') stack.push(-num)
+    else if(op === '*') stack.push(stack.pop() * num)
+    else if(op === '/') stack.push(~~(stack.pop() / num))
+    op = s[i]
+    num = 0
+  }
+}
+  
+return stack.reduce((ac, e) => ac + e, 0)   
+};
