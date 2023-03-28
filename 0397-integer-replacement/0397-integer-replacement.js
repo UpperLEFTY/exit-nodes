6bb4a9d7c3ad3d76a026e25542e56615cdc0b6f2
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 14:43:12 -0700

    Time: 58 ms (80.00%), Space: 43.8 MB (15.00%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const integerReplacement = function(n, memo = {}) {
+    if (n === 1) return 0;
+    if (memo[n]) return memo[n];
+    memo[n] = n % 2 === 0 ? integerReplacement(n/2, memo) + 1 : Math.min(integerReplacement(n+1, memo), integerReplacement(n-1, memo)) + 1;
+      return memo[n]; 
+};
