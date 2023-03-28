c75060267c1e9a9d36784608c1c7775a75dcdbf4
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 14:52:47 -0700

    Time: 78 ms (89.81%), Space: 46.2 MB (83.42%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLongestSubstring = function(s) {
+ let currentString = [];
+    let longestStringLength = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        // get chars position in the current string
+        const currentCharacterPosition = currentString.indexOf(s[i]);
+
+        // need to check if the current character exists in the current string
+        if (currentCharacterPosition !== -1) {
+            // splice after the occurence of the character
+            currentString.splice(0, currentCharacterPosition + 1);
+        }
+        // add character to the array
+        currentString.push(s[i]);
+
+        // Store string length if bigger than the existing record
+        longestStringLength = Math.max(
+            longestStringLength,
+            currentString.length
+        );
+    }
+
+    return longestStringLength;
+};
