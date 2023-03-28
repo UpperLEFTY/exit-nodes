d39f4276e6a49abc27aff1db7351b19ccb2f5cb9
Essence Alexander <upperlefty@zoho.eu>
2023-03-27 17:27:33 -0700

    Time: 75 ms (49.54%), Space: 45.7 MB (16.21%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.js
+++ 0260-single-number-iii/0260-single-number-iii.js
@@ -3,14 +3,23 @@
  * @return {number[]}
  */
 const singleNumber = function(nums) {
-  const hash = {};
-  nums.forEach((el, idx) => {
-    if (hash.hasOwnProperty(el)) {
-      hash[el] += 1;
-      delete hash[el];
-    } else {
-      hash[el] = 1;
-    }
-  });
-  return Object.keys(hash).map(el => +el);
+//   const hash = {};
+//   nums.forEach((el, idx) => {
+//     if (hash.hasOwnProperty(el)) {
+//       hash[el] += 1;
+//       delete hash[el];
+//     } else {
+//       hash[el] = 1;
+//     }
+//   });
+//   return Object.keys(hash).map(el => +el);
+// };
+    
+    /** ANOTHER SOLUTION */
+  const s = new Set()
+ for(let el of nums) {
+   if(s.has(el)) s.delete(el)
+   else s.add(el)
+ }
+ return Array.from(s)
 };
