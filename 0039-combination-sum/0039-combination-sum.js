fadf4960f59b402611b59ed6b1edc03da614feed
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 15:18:13 -0700

    Time: 62 ms (98.75%), Space: 45.7 MB (38.80%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+const combinationSum = function(candidates, target) {
+  let res = [];
+  let len = candidates.length;
+  candidates.sort((a, b) => (a - b));
+  dfs(res, [], 0, len, candidates, target);
+  return res;
+};
+
+const dfs = function (res, stack, index, len, candidates, target) {
+  let tmp = null;
+  if (target < 0) return;
+  if (target === 0) return res.push(stack);
+  for (let i = index; i < len; i++) {
+    if (candidates[i] > target) break;
+    tmp = Array.from(stack);
+    tmp.push(candidates[i]);
+    dfs(res, tmp, i, len, candidates, target - candidates[i]);
+  }
+};
