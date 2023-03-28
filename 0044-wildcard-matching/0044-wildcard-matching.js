fc87ffa49c1a00a4de74f2aba678be1c5efe1fc4
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 15:21:12 -0700

    Time: 178 ms (29.38%), Space: 61.8 MB (11.87%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+const isMatch = function(s, p) {
+  let dp = Array(p.length + 1).fill(0).map(_ => ({}));
+  return test(s, p, 0, 0, dp);
+}; 
+
+const test = function (s, p, sIndex, pIndex, dp) {
+  if (dp[pIndex][sIndex] !== undefined) return dp[pIndex][sIndex];
+    
+  let sNow = s[sIndex];
+  let pNow = p[pIndex];
+  let res = false;
+
+  if (pNow === undefined) return sNow === undefined;
+  if (sNow === undefined) {
+    for (var i = pIndex; i < p.length; i++) {
+      if (p[i] !== '*') return false;
+    }
+    return true;
+  }
+
+  if (sNow === pNow || pNow === '?') {
+    res = test(s, p, sIndex + 1, pIndex + 1, dp);
+  } else if (pNow === '*') {
+    res = test(s, p, sIndex, pIndex + 1, dp) || test(s, p, sIndex + 1, pIndex + 1, dp) || test(s, p, sIndex + 1, pIndex, dp);
+  }
+
+  dp[pIndex][sIndex] = res;
+
+  return res;
+};
