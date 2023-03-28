1962b8684bb2784b9d1fb6b1b7285043c1794062
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 15:37:42 -0700

    Time: 46 ms (97.98%), Space: 42.6 MB (7.14%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,10 +2,20 @@
  * @param {string} s
  * @return {number}
  */
+// const lengthOfLastWord = function(s) {
+//     const str = s.trim();
+//     let len = str.length;
+//     let i = len - 1;
+//     while (i >= 0 && str[i] !== ' ') i--;
+//     return len - 1 - i;
+// };
+
+        /** ANOTHER SOLUTION */
+
 const lengthOfLastWord = function(s) {
-    const str = s.trim();
-    let len = str.length;
-    let i = len - 1;
-    while (i >= 0 && str[i] !== ' ') i--;
-    return len - 1 - i;
+  const arr = s.split(" ");
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i].length > 0) return arr[i].length;
+  }
+  return 0;
 };
