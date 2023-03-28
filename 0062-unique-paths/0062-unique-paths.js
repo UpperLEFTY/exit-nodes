1c503dcdcf662c6aca725227fa18750a39e95521
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 15:40:45 -0700

    Time: 53 ms (93.42%), Space: 41.6 MB (92.07%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+const uniquePaths = function(m, n) {
+const dp = Array(m).fill(0)
+  for(let i = 0; i < n; i++) {
+    dp[0] = 1
+    for(let j = 1; j < m; j++) {
+      dp[j] += dp[j - 1]
+    }
+  }
+  return dp[m - 1]
+};
