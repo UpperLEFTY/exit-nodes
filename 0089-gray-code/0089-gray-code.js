e26b0a2c9fd07cf637866df436c45b08692d2a18
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 17:25:40 -0700

    Time: 99 ms (79.07%), Space: 52.7 MB (44.96%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+const grayCode = function(n) {
+   if (n === 0) {
+  return [0]
+}
+const temp = grayCode(n - 1)
+const nums = [].concat(temp)
+const addNum = 1 << (n - 1)
+for (let i = temp.length - 1; i >= 0; i--) {
+  nums.push(addNum + temp[i])
+}
+return nums
+}
