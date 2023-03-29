8b1fe60ffb7f3f01c15870ae7aecd4df23c68581
Essence Alexander <upperlefty@zoho.eu>
2023-03-29 01:03:47 -0700

    Time: 77 ms (95.42%), Space: 52.1 MB (78.17%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+const maxProfit = function(prices) {
+  let buy1 = Number.MIN_SAFE_INTEGER;
+  let sell1 = 0;
+  let buy2 = Number.MIN_SAFE_INTEGER;
+  let sell2 = 0;
+  let len = prices.length;
+  for (let i = 0; i < len; i++) {
+    buy1 = Math.max(buy1, -prices[i]);
+    sell1 = Math.max(sell1, buy1 + prices[i]);
+    buy2 = Math.max(buy2, sell1 - prices[i]);
+    sell2 = Math.max(sell2, buy2 + prices[i]);
+  }
+  return sell2;
+};
