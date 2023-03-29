bdec7661c6b6fa731cf12f6881d0db0fd0fdc511
Essence Alexander <upperlefty@zoho.eu>
2023-03-29 01:04:59 -0700

    Time: 75 ms (87.62%), Space: 51.6 MB (49.66%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxPathSum = function(root) {
+  let obj = {
+    max: Number.MIN_SAFE_INTEGER
+  }
+  traverse(root, obj)
+  return obj.max
+};
+
+function traverse(node, obj) {
+  if(node === null) return 0
+  let left = Math.max(0, traverse(node.left, obj))
+  let right = Math.max(0, traverse(node.right, obj))
+  obj.max = Math.max(obj.max, node.val+left+right)
+  return node.val + Math.max(left, right)
+};
