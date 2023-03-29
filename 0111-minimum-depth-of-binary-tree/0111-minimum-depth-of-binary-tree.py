640aeaa4bd4f112c4ad764b8d448d3bf93f03092
Essence Alexander <upperlefty@zoho.eu>
2023-03-29 00:38:28 -0700

    Time: 449 ms (99.68%), Space: 49.3 MB (91.89%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -4,12 +4,35 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# first iteration: Top-down DFS
+# class Solution:
+#   def minDepth(self, root: Optional[TreeNode]) -> int:
+#     if not root:
+#       return 0
+#     if not root.left:
+#       return self.minDepth(root.right) + 1
+#     if not root.right:
+#       return self.minDepth(root.left) + 1
+#     return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
+# second iteration: Bottom-up BFS
+
 class Solution:
   def minDepth(self, root: Optional[TreeNode]) -> int:
     if not root:
       return 0
-    if not root.left:
-      return self.minDepth(root.right) + 1
-    if not root.right:
-      return self.minDepth(root.left) + 1
-    return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
+    ans = 0
+    q = collections.deque([root])
+
+    while q:
+      ans += 1
+      for _ in range(len(q)):
+        node = q.popleft()
+        if not node.left and not node.right:
+          return ans
+        if node.left:
+          q.append(node.left)
+        if node.right:
+          q.append(node.right)
+
