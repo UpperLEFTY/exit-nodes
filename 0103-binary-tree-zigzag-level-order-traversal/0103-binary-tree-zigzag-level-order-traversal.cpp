c91ce39e6a773340d2c879cc6bda891ad41ca641
Essence Alexander <upperlefty@zoho.eu>
2023-03-29 00:13:39 -0700

    Time: 3 ms (71.96%), Space: 12.3 MB (17.53%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -9,6 +9,48 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+//  public:
+//   vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+//     if (root == nullptr)
+//       return {};
+
+//     vector<vector<int>> ans;
+//     deque<TreeNode*> q{{root}};
+//     bool isLeftToRight = true;
+
+//     while (!q.empty()) {
+//       vector<int> currLevel;
+//       for (int sz = q.size(); sz > 0; --sz)
+//         if (isLeftToRight) {
+//           TreeNode* node = q.front();
+//           q.pop_front();
+//           currLevel.push_back(node->val);
+//           if (node->left)
+//             q.push_back(node->left);
+//           if (node->right)
+//             q.push_back(node->right);
+//         } else {
+//           TreeNode* node = q.back();
+//           q.pop_back();
+//           currLevel.push_back(node->val);
+//           if (node->right)
+//             q.push_front(node->right);
+//           if (node->left)
+//             q.push_front(node->left);
+//         }
+//       ans.push_back(currLevel);
+//       isLeftToRight = !isLeftToRight;
+//     }
+
+//     return ans;
+//   }
+// };
+
+
+            // ANOTHER 
+
+
 class Solution {
  public:
   vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -16,29 +58,22 @@ class Solution {
       return {};
 
     vector<vector<int>> ans;
-    deque<TreeNode*> q{{root}};
+    queue<TreeNode*> q{{root}};
     bool isLeftToRight = true;
 
     while (!q.empty()) {
-      vector<int> currLevel;
-      for (int sz = q.size(); sz > 0; --sz)
-        if (isLeftToRight) {
-          TreeNode* node = q.front();
-          q.pop_front();
-          currLevel.push_back(node->val);
-          if (node->left)
-            q.push_back(node->left);
-          if (node->right)
-            q.push_back(node->right);
-        } else {
-          TreeNode* node = q.back();
-          q.pop_back();
-          currLevel.push_back(node->val);
-          if (node->right)
-            q.push_front(node->right);
-          if (node->left)
-            q.push_front(node->left);
-        }
+      const int size = q.size();
+      vector<int> currLevel(size);
+      for (int i = 0; i < size; ++i) {
+        TreeNode* node = q.front();
+        q.pop();
+        const int index = isLeftToRight ? i : size - i - 1;
+        currLevel[index] = node->val;
+        if (node->left)
+          q.push(node->left);
+        if (node->right)
+          q.push(node->right);
+      }
       ans.push_back(currLevel);
       isLeftToRight = !isLeftToRight;
     }
@@ -46,3 +81,4 @@ class Solution {
     return ans;
   }
 };
+
