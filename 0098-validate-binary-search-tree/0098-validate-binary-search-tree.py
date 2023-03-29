01528468b99bad1270327b11441a980846b06ce2
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 20:12:40 -0700

    Time: 54 ms (17.73%), Space: 16.2 MB (95.48%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,18 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#   def isValidBST(self, root: Optional[TreeNode]) -> bool:
+#     def isValidBST(root: Optional[TreeNode],
+#                    minNode: Optional[TreeNode], maxNode: Optional[TreeNode]) -> bool:
+#       if not root:
+#         return True
+#       if minNode and root.val <= minNode.val:
+#         return False
+#       if maxNode and root.val >= maxNode.val:
+#         return False
+
+#       return isValidBST(root.left, minNode, root) and \
+#           isValidBST(root.right, root, maxNode)
+
+#     return isValidBST(root, None, None)
+
+
+            # ANOTHER SOLUTION
 class Solution:
   def isValidBST(self, root: Optional[TreeNode]) -> bool:
-    def isValidBST(root: Optional[TreeNode],
-                   minNode: Optional[TreeNode], maxNode: Optional[TreeNode]) -> bool:
-      if not root:
-        return True
-      if minNode and root.val <= minNode.val:
-        return False
-      if maxNode and root.val >= maxNode.val:
-        return False
+    stack = []
+    pred = None
 
-      return isValidBST(root.left, minNode, root) and \
-          isValidBST(root.right, root, maxNode)
+    while root or stack:
+      while root:
+        stack.append(root)
+        root = root.left
+      root = stack.pop()
+      if pred and pred.val >= root.val:
+        return False
+      pred = root
+      root = root.right
 
-    return isValidBST(root, None, None)
+    return True
