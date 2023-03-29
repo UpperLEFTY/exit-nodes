8541103e951590ae06a9c9ba02a2558c8474a3b1
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 17:51:20 -0700

    Time: 3 ms (57.75%), Space: 7.4 MB (68.14%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -8,27 +8,54 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+//  public:
+//   ListNode* reverseBetween(ListNode* head, int left, int right) {
+//     if (left == 1)
+//       return reverseN(head, right);
+
+//     head->next = reverseBetween(head->next, left - 1, right - 1);
+
+//     return head;
+//   }
+
+//  private:
+//   ListNode* reverseN(ListNode* head, int n) {
+//     if (n == 1)
+//       return head;
+
+//     ListNode* newHead = reverseN(head->next, n - 1);
+//     ListNode* headNext = head->next;
+//     head->next = headNext->next;
+//     headNext->next = head;
+
+//     return newHead;
+//   }
+// };
+                // ANOTHER SOLUTION
+
 class Solution {
  public:
-  ListNode* reverseBetween(ListNode* head, int left, int right) {
-    if (left == 1)
-      return reverseN(head, right);
-
-    head->next = reverseBetween(head->next, left - 1, right - 1);
-
-    return head;
-  }
-
- private:
-  ListNode* reverseN(ListNode* head, int n) {
-    if (n == 1)
+  ListNode* reverseBetween(ListNode* head, int m, int n) {
+    if (!head || m == n)
       return head;
 
-    ListNode* newHead = reverseN(head->next, n - 1);
-    ListNode* headNext = head->next;
-    head->next = headNext->next;
-    headNext->next = head;
+    ListNode dummy(0, head);
+    ListNode* prev = &dummy;
 
-    return newHead;
+    for (int i = 0; i < m - 1; ++i)
+      prev = prev->next;  // Point to the node before the sublist [m, n]
+
+    ListNode* tail = prev->next;  // Will be the tail of the sublist [m, n]
+
+    // Reverse the sublist [m, n] one by one
+    for (int i = 0; i < n - m; ++i) {
+      ListNode* cache = tail->next;
+      tail->next = cache->next;
+      cache->next = prev->next;
+      prev->next = cache;
+    }
+
+    return dummy.next;
   }
 };
