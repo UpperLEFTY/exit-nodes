e4344940d375935f399eff8c5786a0fa2729aa70
Essence Alexander <upperlefty@zoho.eu>
2023-03-29 00:48:37 -0700

    Time: 23 ms (94.75%), Space: 6.4 MB (97.24%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,20 +1,40 @@
+// class Solution {
+//  public:
+//   int numDistinct(string s, string t) {
+//     const int m = s.length();
+//     const int n = t.length();
+//     vector<vector<unsigned long>> dp(m + 1, vector<unsigned long>(n + 1));
+
+//     for (int i = 0; i <= m; ++i)
+//       dp[i][0] = 1;
+
+//     for (int i = 1; i <= m; ++i)
+//       for (int j = 1; j <= n; ++j)
+//         if (s[i - 1] == t[j - 1])
+//           dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+//         else
+//           dp[i][j] = dp[i - 1][j];
+
+//     return dp[m][n];
+//   }
+// };
+
+        // ANOTHER SOLUTION 
+
+
 class Solution {
  public:
   int numDistinct(string s, string t) {
     const int m = s.length();
     const int n = t.length();
-    vector<vector<unsigned long>> dp(m + 1, vector<unsigned long>(n + 1));
-
-    for (int i = 0; i <= m; ++i)
-      dp[i][0] = 1;
+    vector<unsigned long> dp(n + 1);
+    dp[0] = 1;
 
     for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
+      for (int j = n; j >= 1; --j)
         if (s[i - 1] == t[j - 1])
-          dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-        else
-          dp[i][j] = dp[i - 1][j];
+          dp[j] += dp[j - 1];
 
-    return dp[m][n];
+    return dp[n];
   }
 };
