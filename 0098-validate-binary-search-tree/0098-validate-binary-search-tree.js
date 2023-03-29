7402477473d3ffe5966f6952382ba4a62998c4c5
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 20:11:12 -0700

    Time: 62 ms (93.96%), Space: 46.9 MB (22.12%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isValidBST = function(root) {
+ return helper(root, -Infinity, Infinity)
+}
+function helper(root, minValue, maxValue) {
+ if (!root) return true
+ if (root.val <= minValue || root.val >= maxValue) {
+   return false
+ }
+ let leftSide = helper(root.left, minValue, root.val)
+ let rightSide = helper(root.right, root.val, maxValue)
+ return leftSide && rightSide
+}
