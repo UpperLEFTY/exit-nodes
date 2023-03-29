0d05646809e533b0221515513d905104ecdd3a8e
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 17:16:50 -0700

    Time: 71 ms (43.43%), Space: 44.1 MB (22.31%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+const partition = function(head, x) {
+  const leftHead = new ListNode(); 
+  const rightHead = new ListNode(); 
+  let left = leftHead;
+  let right = rightHead;
+  
+  // split list into two sides, left if val < x, else right
+  for (let node = head; node !== null; node = node.next) {
+    if (node.val < x) {
+      left.next = node;
+      left = node;
+    } else {
+      right.next = node;
+      right = node;
+    }
+  }
+  
+  // combine the two sides
+  left.next = rightHead.next;
+  right.next = null;
+  
+  return leftHead.next;
+};
