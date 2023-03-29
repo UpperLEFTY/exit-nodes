cc82608ebd49ffecb24ff138d4206dcf211afac9
Essence Alexander <upperlefty@zoho.eu>
2023-03-29 00:14:27 -0700

    Time: 34 ms (65.91%), Space: 14.2 MB (10.40%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -4,6 +4,39 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#   def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#     if not root:
+#       return []
+
+#     ans = []
+#     q = collections.deque([root])
+#     isLeftToRight = True
+
+#     while q:
+#       currLevel = []
+#       for _ in range(len(q)):
+#         if isLeftToRight:
+#           node = q.popleft()
+#           currLevel.append(node.val)
+#           if node.left:
+#             q.append(node.left)
+#           if node.right:
+#             q.append(node.right)
+#         else:
+#           node = q.pop()
+#           currLevel.append(node.val)
+#           if node.right:
+#             q.appendleft(node.right)
+#           if node.left:
+#             q.appendleft(node.left)
+#       ans.append(currLevel)
+#       isLeftToRight = not isLeftToRight
+
+#     return ans
+
+            # ANOTHER
+
 class Solution:
   def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
     if not root:
@@ -14,22 +47,16 @@ class Solution:
     isLeftToRight = True
 
     while q:
-      currLevel = []
-      for _ in range(len(q)):
-        if isLeftToRight:
-          node = q.popleft()
-          currLevel.append(node.val)
-          if node.left:
-            q.append(node.left)
-          if node.right:
-            q.append(node.right)
-        else:
-          node = q.pop()
-          currLevel.append(node.val)
-          if node.right:
-            q.appendleft(node.right)
-          if node.left:
-            q.appendleft(node.left)
+      size = len(q)
+      currLevel = [0] * size
+      for i in range(size):
+        node = q.popleft()
+        index = i if isLeftToRight else size - i - 1
+        currLevel[index] = node.val
+        if node.left:
+          q.append(node.left)
+        if node.right:
+          q.append(node.right)
       ans.append(currLevel)
       isLeftToRight = not isLeftToRight
 
