bfb69430e8c9180928d71114158c6ba03b572e5a
Essence Alexander <upperlefty@zoho.eu>
2023-03-28 17:58:01 -0700

    Time: 72 ms (12.21%), Space: 42.3 MB (38.25%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+const inorderTraversal = function(root) {
+    let res = [];
+    sendhelp(root, res);
+    return res; 
+};
+
+  let sendhelp = function (root, res) {
+    if (!root) return;
+    if (root.left) sendhelp(root.left, res);
+    res.push(root.val);
+    if (root.right) sendhelp(root.right, res);
+}
