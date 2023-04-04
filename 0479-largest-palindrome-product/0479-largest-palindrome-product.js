539d95b94bea913a2c484e562e3e18417e66e881
Essence Alexander <upperlefty@zoho.eu>
2023-04-04 10:50:29 -0700

    Time: 6225 ms (14.29%), Space: 47.1 MB (14.29%) - LeetHub

---
+++ 0479-largest-palindrome-product/0479-largest-palindrome-product.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */ 
+const largestPalindrome = function (n) {
+  if(n === 1) return 9
+  let max = BigInt(10 ** n - 1), min = max / 10n + 1n
+  for(let h = max; h >= min; h--) {
+    let left = h, right = 0n
+    for(let i = h; i !== 0n; ) {
+      right = right * 10n + i % 10n
+      i = i / 10n
+      left *= 10n
+    }
+    let pal = left + right
+    for(let i = max; i >= min; i--) {
+      let j = pal / i
+      if(j > i) break
+      if(pal % i === 0n) return pal % 1337n
+    }
+  }
+}
