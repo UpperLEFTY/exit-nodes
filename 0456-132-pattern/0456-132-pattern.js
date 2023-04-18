8da9bac1a0ef3da19b15423b58e92efbf62e9783
Essence Alexander <upperlefty@zoho.eu>
2023-04-17 23:47:59 -0700

    Time: 61 ms (97.47%), Space: 48.5 MB (100.00%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.js
+++ 0456-132-pattern/0456-132-pattern.js
@@ -1,17 +1,39 @@
+// /**
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// const find132pattern = function(nums) {
+//   let [stack, s3] = [[], -Infinity]
+//   for (let i = nums.length - 1; i >= 0; i--) {
+//     if (nums[i] < s3) {
+//       return true
+//     }
+//     while (stack[stack.length - 1] < nums[i]) {
+//       s3 = stack.pop()
+//     }
+//     stack.push(nums[i])
+//   }
+//   return false
+// }
+
+
+
+//----------------Solution #2--------------------------\\
+
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 const find132pattern = function(nums) {
-  let [stack, s3] = [[], -Infinity]
-  for (let i = nums.length - 1; i >= 0; i--) {
-    if (nums[i] < s3) {
-      return true
+  let idx = nums.length
+  let s3 = Number.NEGATIVE_INFINITY
+  for(let len = nums.length, i = len - 1; i >= 0; i--) {
+    if(nums[i] < s3) return true
+    while(idx < nums.length && nums[i] > nums[idx]) {
+      s3 = nums[idx++]
     }
-    while (stack[stack.length - 1] < nums[i]) {
-      s3 = stack.pop()
-    }
-    stack.push(nums[i])
+    nums[--idx] = nums[i] 
   }
   return false
 }
+
