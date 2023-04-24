e6adc59432bfeab1f91c12d9c9c968eb7faa29eb
Essence Alexander <upperlefty@zoho.eu>
2023-04-23 19:01:54 -0700

    Time: 57 ms (83.26%), Space: 41.8 MB (97.83%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.js
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -2,11 +2,11 @@
  * @param {number[]} stones
  * @return {number}
  */
-var lastStoneWeight = function(stones) {
+const lastStoneWeight = function(stones) {
   stones.sort((a, b) => a - b)
-while (stones.length > 1) {
-  const num = Math.abs(stones.pop() - stones.pop())
-  stones.splice(stones.findIndex(item => item >= num), 0, num)
-}
-return stones[0] 
+  while (stones.length > 1) {
+    const num = Math.abs(stones.pop() - stones.pop())
+    stones.splice(stones.findIndex(item => item >= num), 0, num)
+  }
+  return stones[0] 
 };
