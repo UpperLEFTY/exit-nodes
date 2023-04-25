5e576aadabf54f88b93471fe9499cb446e21ca98
Essence Alexander <upperlefty@zoho.eu>
2023-04-25 07:05:39 -0700

    Time: 369 ms (61.48%), Space: 18 MB (29.26%) - LeetHub

---
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+    ans = 0
+
+    def longestUnivaluePathDownFrom(root: Optional[TreeNode]) -> int:
+      nonlocal ans
+      if not root:
+        return 0
+
+      l = longestUnivaluePathDownFrom(root.left)
+      r = longestUnivaluePathDownFrom(root.right)
+      arrowLeft = l + 1 if root.left and root.left.val == root.val else 0
+      arrowRight = r + 1 if root.right and root.right.val == root.val else 0
+      ans = max(ans, arrowLeft + arrowRight)
+      return max(arrowLeft, arrowRight)
+
+    longestUnivaluePathDownFrom(root)
+    return ans
