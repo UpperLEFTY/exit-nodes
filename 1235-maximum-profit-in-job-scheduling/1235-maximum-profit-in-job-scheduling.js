2cb7afcfbdd4f2553087a8a439b00523ebcc4f09
Essence Alexander <upperlefty@zoho.eu>
2023-07-11 18:56:36 -0700

    Time: 123 ms (69.05%), Space: 59.4 MB (50.00%) - LeetHub

---
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number[]} profit
+ * @return {number}
+ */
+const jobScheduling = function (startTime, endTime, profit) {
+  const n = startTime.length
+  const items = Array(n)
+  for(let i = 0;i < n; i++) items[i] = [startTime[i], endTime[i], profit[i]]
+  items.sort((a, b) => a[1] - b[1])
+  const dpEndTime = [0]
+  const dpProfit = [0]
+  for(const [s, e, p] of items) {
+    const prevIdx = binarySearch(dpEndTime, 0, dpEndTime.length - 1, s)
+    const curProfit = dpProfit[prevIdx] + p, maxProfit = dpProfit[dpProfit.length - 1]
+    if(curProfit > maxProfit) {
+      dpProfit.push(curProfit)
+      dpEndTime.push(e)
+    }
+  }
+  
+  return dpProfit[dpProfit.length - 1]
+}
+
+function binarySearch(arr, l, r, x) {
+  while (l < r) {
+    const mid = r - ((r - l) >> 1)
+    if (arr[mid] > x) r = mid - 1
+    else l = mid
+  }
+  return l
+}
