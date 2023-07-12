233f1175444f14b4eab9ca47fdf46f85819281d6
Essence Alexander <upperlefty@zoho.eu>
2023-07-11 18:53:55 -0700

    Time: 50 ms (95.24%), Space: 41.9 MB (95.24%) - LeetHub

---
+++ 887-super-egg-drop/887-super-egg-drop.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} k
+ * @param {number} n
+ * @return {number}
+ */
+const superEggDrop = function(K, N) {
+  let lo = 1,
+    hi = N,
+    mi
+  while (lo < hi) {
+    mi = ((lo + hi) / 2) >> 0
+    if (f(mi, K, N) < N) lo = mi + 1
+    else hi = mi
+  }
+  return lo
+}
+
+function f(x, K, N) {
+  let ans = 0,
+    r = 1,
+    i = 1
+  for (let i = 1; i <= K; ++i) {
+    r = ((r * (x - i + 1)) / i) >> 0
+    ans += r
+    if (ans >= N) break
+  }
+  return ans
+}
