d17f260fcb2ef477dbc20ffab8cb5604eb2b8bae
Essence Alexander <upperlefty@zoho.eu>
2023-07-11 23:03:03 -0700

    Time: 235 ms (91.50%), Space: 74.5 MB (83.36%) - LeetHub

---
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const eraseOverlapIntervals = function(intervals) {
+    intervals.sort((a, b) => a[1] - b[1]);
+    let end = intervals[0][1],
+        ans = 0;
+    for (let i = 1; i < intervals.length; ++i) {
+        let cur = intervals[i];
+        if (end > cur[0]) {
+            ans++;
+        } else {
+            end = cur[1];
+        }
+    }
+    return ans;
+};
