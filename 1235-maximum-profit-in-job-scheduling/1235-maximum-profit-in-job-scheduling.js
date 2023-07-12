afea723a9641e00a6e09c24f876fc83d8e7ae90c
Essence Alexander <upperlefty@zoho.eu>
2023-07-11 18:58:12 -0700

    Time: 165 ms (13.09%), Space: 69.8 MB (5.95%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.js
@@ -5,29 +5,66 @@
  * @return {number}
  */
 const jobScheduling = function (startTime, endTime, profit) {
-  const n = startTime.length
-  const items = Array(n)
-  for(let i = 0;i < n; i++) items[i] = [startTime[i], endTime[i], profit[i]]
-  items.sort((a, b) => a[1] - b[1])
-  const dpEndTime = [0]
-  const dpProfit = [0]
-  for(const [s, e, p] of items) {
+//   const n = startTime.length
+//   const items = Array(n)
+//   for(let i = 0;i < n; i++) items[i] = [startTime[i], endTime[i], profit[i]]
+//   items.sort((a, b) => a[1] - b[1])
+//   const dpEndTime = [0]
+//   const dpProfit = [0]
+//   for(const [s, e, p] of items) {
+//     const prevIdx = binarySearch(dpEndTime, 0, dpEndTime.length - 1, s)
+//     const curProfit = dpProfit[prevIdx] + p, maxProfit = dpProfit[dpProfit.length - 1]
+//     if(curProfit > maxProfit) {
+//       dpProfit.push(curProfit)
+//       dpEndTime.push(e)
+//     }
+//   }
+  
+//   return dpProfit[dpProfit.length - 1]
+// }
+
+// function binarySearch(arr, l, r, x) {
+//   while (l < r) {
+//     const mid = r - ((r - l) >> 1)
+//     if (arr[mid] > x) r = mid - 1
+//     else l = mid
+//   }
+//   return l
+// }
+    // ANOTHER SOLUTION 
+  const items = Array.from({ length: startTime.length }, () => Array(3).fill(0))
+  for (let i = 0; i < startTime.length; i++) {
+    items[i] = [startTime[i], endTime[i], profit[i]]
+  }
+  items.sort((a1, a2) => a1[1] - a2[1])
+  const dpEndTime = []
+  const dpProfit = []
+  dpEndTime.push(0)
+  dpProfit.push(0)
+  for (let item of items) {
+    const s = item[0],
+      e = item[1],
+      p = item[2]
+    // find previous endTime index
     const prevIdx = binarySearch(dpEndTime, 0, dpEndTime.length - 1, s)
-    const curProfit = dpProfit[prevIdx] + p, maxProfit = dpProfit[dpProfit.length - 1]
-    if(curProfit > maxProfit) {
-      dpProfit.push(curProfit)
+    const currProfit = dpProfit[prevIdx] + p,
+      maxProfit = dpProfit[dpProfit.length - 1]
+    if (currProfit > maxProfit) {
+      dpProfit.push(currProfit)
       dpEndTime.push(e)
     }
   }
-  
   return dpProfit[dpProfit.length - 1]
 }
 
 function binarySearch(arr, l, r, x) {
-  while (l < r) {
-    const mid = r - ((r - l) >> 1)
+  while (l <= r) {
+    const mid = l + ((r - l) >> 1)
     if (arr[mid] > x) r = mid - 1
-    else l = mid
+    else {
+      if (mid == arr.length - 1 || arr[mid + 1] > x) return mid
+      l = mid + 1
+    }
   }
-  return l
+  return -1
 }
