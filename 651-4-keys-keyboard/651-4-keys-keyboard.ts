948861f6059b9712009e6774341334be9798b7b0
Essence Alexander <upperlefty@zoho.eu>
2023-07-13 22:21:14 -0700

    Time: 69 ms (20.00%), Space: 43.1 MB (80.00%) - LeetHub

---
+++ 651-4-keys-keyboard/651-4-keys-keyboard.ts
@@ -0,0 +1,13 @@
+function maxA(n: number): number {
+   const dp = [0, 1, 2, 3, 4, 5, 6] 
+   const recursive = function (n) {
+       if(dp[n]) return dp[n]
+        const max = Math.max(
+        recursive(n - 3) * 2,
+        recursive(n - 4) * 3,
+        recursive(n - 5) * 4
+        )
+        return (dp[n] = max)
+   }
+   return recursive(n)
+};
