79c7f412d8cadf85a3abacae82c17bf2743dda06
Essence Alexander <upperlefty@zoho.eu>
2023-07-15 16:32:05 -0700

    Time: 80 ms (10.34%), Space: 46.1 MB (10.34%) - LeetHub

---
+++ 256-paint-house/256-paint-house.ts
@@ -0,0 +1,11 @@
+function minCost(costs: number[][]): number {
+     let [a, b, c] = [0, 0, 0];
+    for (let [ca, cb, cc] of costs) {
+        [a, b, c] = [
+            Math.min(b, c) + ca,
+            Math.min(a, c) + cb,
+            Math.min(a, b) + cc,
+        ];
+    }
+    return Math.min(a, b, c);
+};
