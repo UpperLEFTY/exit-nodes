5d900b9e055be7ab80ca8f6f89e4d668b425a1c2
Essence Alexander <upperlefty@zoho.eu>
2023-07-15 16:43:14 -0700

    Time: 96 ms (48.28%), Space: 53.7 MB (19.53%) - LeetHub

---
+++ 896-monotonic-array/896-monotonic-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const isMonotonic = function(nums) {
+    const n = nums.length;
+let isOrder = false;
+let isDecs = false;
+for (let i = 1; i < n; i++) {
+    const pre = nums[i - 1];
+    const cur = nums[i];
+    if (pre < cur) {
+        isOrder = true;
+    } else if (pre > cur) {
+        isDecs = true;
+    }
+    if (isOrder && isDecs) {
+        return false;
+    }
+}
+return true;
+};
