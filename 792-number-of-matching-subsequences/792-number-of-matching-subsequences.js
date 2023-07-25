664bbf57252234035990a9f20b46955d5860347c
Essence Alexander <upperlefty@zoho.eu>
2023-07-24 22:32:03 -0700

    Time: 233 ms (40.26%), Space: 57 MB (23.38%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number}
+ */
+const numMatchingSubseq = function(s, words) {
+  const hash = {}
+  for(let w of words) {
+    if(hash[w[0]] == null) hash[w[0]] = []
+    const it = w[Symbol.iterator]()
+    hash[w[0]].push( it )
+    it.next()
+  }
+  let res = 0
+  for(let ch of s) {
+    const advance = hash[ch] || []
+    hash[ch] = []
+    for(let it of advance) {
+      const obj = it.next()
+      if(obj.done === false) {
+        if(hash[obj.value] == null) hash[obj.value] = []
+        hash[obj.value].push(it)
+      } else {
+        res++
+      }
+    }
+  }
+
+  return res
+};
