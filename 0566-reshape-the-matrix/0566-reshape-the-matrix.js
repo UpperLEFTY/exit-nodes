c40ec929d4d114f2714b245d345c37a7e5666417
Essence Alexander <upperlefty@zoho.eu>
2023-09-30 21:08:24 -0700

    Time: 54 ms (98.67%), Space: 44.8 MB (79.56%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function(mat, r, c) {
+  if (isValid(mat, r, c) === false) {
+    return mat;
+  }
+  const arr = [];
+  mat.forEach(el => arr.push(...el));
+  const res = [];
+  for (let start = 0; start < arr.length; start = start + c) {
+    res.push(arr.slice(start, start + c));
+  }
+  return res;
+};
+
+function isValid(mat, r, c) {
+  if (mat.length * mat[0].length !== r * c) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(matrixReshape([[1, 2], [3, 4]], 1, 4));
+console.log(matrixReshape([[1, 2], [3, 4]], 2, 4));
