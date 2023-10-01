430a65c153e72f9728fdfd8ca0e75e98d4bc05bc
Essence Alexander <upperlefty@zoho.eu>
2023-09-30 20:49:43 -0700

    Time: 52 ms (96.42%), Space: 49 MB (22.56%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseWords = function(s) {
+    return s.split(' ').map(reverse).join(' ');
+};
+const reverse = function (word) {
+  let len = word.length;
+  let res = '';
+  for (let i = 0; i < len; i++) {
+    res += word[len - i - 1];
+  }
+  return res;
+};
+
