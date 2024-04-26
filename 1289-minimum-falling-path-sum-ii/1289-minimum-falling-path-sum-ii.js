c7bfd4d6db0e104ae88c2149341bdab3470afac9
Essence Alexander <upperlefty@zoho.eu>
2024-04-26 10:09:01 -0700

    Time: 73 ms (71.43%), Space: 52.4 MB (85.71%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.js
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.js
@@ -1,21 +1,49 @@
+// /**
+//  * @param {number[][]} grid
+//  * @return {number}
+//  */
+// const minFallingPathSum = function (grid) {
+//   const n = grid.length
+//   for (let i = 1; i < n; i++) {
+//     const [m1, m2] = min2(grid[i - 1])
+//     for (j = 0; j < n; j++) {
+//       grid[i][j] += grid[i - 1][j] !== m1 ? m1 : m2
+//     }
+//   }
+//   return Math.min(...grid[n - 1])
+// }
+
+// function min2(grid) {
+//   let m1 = Infinity, m2 = Infinity
+//   grid.forEach(e => {
+//     if (e < m1) m2 = m1, m1 = e
+//     else if (e < m2) m2 = e
+//   })
+//   return [m1, m2]
+// }
+
+
+// ANOTHER BUT WITH ARRAY
+
+
 /**
- * @param {number[][]} grid
+ * @param {number[][]} arr
  * @return {number}
  */
-const minFallingPathSum = function (grid) {
-  const n = grid.length
+const minFallingPathSum = function (arr) {
+  const n = arr.length
   for (let i = 1; i < n; i++) {
-    const [m1, m2] = min2(grid[i - 1])
+    const [m1, m2] = min2(arr[i - 1])
     for (j = 0; j < n; j++) {
-      grid[i][j] += grid[i - 1][j] !== m1 ? m1 : m2
+      arr[i][j] += arr[i - 1][j] !== m1 ? m1 : m2
     }
   }
-  return Math.min(...grid[n - 1])
+  return Math.min(...arr[n - 1])
 }
 
-function min2(grid) {
+function min2(arr) {
   let m1 = Infinity, m2 = Infinity
-  grid.forEach(e => {
+  arr.forEach(e => {
     if (e < m1) m2 = m1, m1 = e
     else if (e < m2) m2 = e
   })
