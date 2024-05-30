0318efdf81828a717d2b9f390df010b0eb43ffb9
Essence Alexander <upperlefty@zoho.eu>
2024-05-30 16:25:30 -0700

    Time: 47 ms (85.71%), Space: 48.3 MB (100.00%) - LeetHub

---
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const countTriplets = function(arr) {
+  let res = 0
+  const n = arr.length
+  for(let i = 0; i < n; i++) {
+    let xor = arr[i]
+    for(let j = i + 1; j < n; j++) {
+      xor ^= arr[j]
+      if(xor === 0) res += j - i
+    }
+  }
+  
+  return res
+};
